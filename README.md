f49d0d63550f08a3d86f50a81343584e5aa3356e
Corrie Bartelheimer <corrie.b@web.de>
2019-01-21 17:08:41 +0100

    update slides and readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Estimating-Houseprices
+# Estimating Houseprices with RStanArm
+
+This repo contains my slides and necessary code.
+
+The results are based on data by Europace and were also published [here](https://tech.europace.de/hauspreise-in-berlin/) (in German).
+
+
+For some visualizations, I use Shapefiles and some public ZIP code data which can be obtained [here (Shapefiles)](https://www.suche-postleitzahl.org/download_files/public/plz-gebiete.shp.zip) and [here (ZIP Code data)](https://www.berlin.de/senuvk/umwelt/luftqualitaet/umweltzone/download/umweltzone_plz_zuordnung.xls).
---
+++ script.R
@@ -0,0 +1,101 @@
+library(tidyverse)
+library(rstanarm)
+library(loo)
+options(mc.cores = parallel::detectCores())
+
+load("data/df_raw.rda")  # load raw data frame df
+set.seed(2405)
+
+##########################################################################################################
+#### Some preprocessing
+## removing NA values
+df <- df %>% 
+  filter(gesamtwohnflaeche > 0) %>%
+  mutate_at(c("herstellung", "grundstueckskaufpreis", 
+              "kaufpreis", "modernisierung"), funs(replace(., is.na(.), 0))) %>%
+  mutate(gesamt_kaufpreis = herstellung + grundstueckskaufpreis + kaufpreis + modernisierung,
+         qm_preis =  gesamt_kaufpreis / gesamtwohnflaeche)
+
+#######################################################
+### filter out test case: 
+## removing all properties that have as address the old or new company's address
+df <- df %>%
+  filter(strasse != "Klosterstraße" &
+           !str_detect(produktanbieterid, "MUSTER") &
+           !(hausnummer==71 & plz=="10179") &
+           !(hausnummer == 77 & plz == "10247"))
+
+########################################################
+#### filter out extreme outlier cases
+
+df.m  <- df %>%
+  filter(grundstuecksgroesse < 5e4 &
+           gesamtwohnflaeche < 500 &
+           gesamt_kaufpreis < 3e6) 
+
+
+#########################################################
+#### last preprocessing: scaling
+
+df.model <- df.m %>% 
+  select(gesamtwohnflaeche,  gesamt_kaufpreis, plz) %>%
+  rename(area = gesamtwohnflaeche, price = gesamt_kaufpreis) %>%
+  mutate(area.s = as.numeric(scale(area, scale=T)),
+         area.c = area - mean(area),
+         price.s = price / 100000) %>%
+  as.tibble
+
+
+
+##########################################################################################################
+### run the models
+mod_base <-   stan_lmer( price.s ~  area.c + 
+                           (1 + area.c | plz)  ,
+                         data=df.model,
+                         prior_intercept=normal(location=3, scale=3, autoscale = F),
+                         prior=normal(location=0, scale=2.5, autoscale=F)) 
+
+mod_flat <-   stan_lmer( price.s ~  area.c + 
+                           (1 + area.c | plz)  ,
+                         data=df.model,
+                         prior_intercept=NULL,
+                         prior=NULL) 
+
+mod_base_default_prior <-   stan_lmer( price.s ~  area + (1 + area | plz)  ,
+                    data=df.model)
+
+mod_simple <- stan_glm( price.s ~  area.c   ,
+                        data=df.model)
+
+
+######################################
+## compute loo values
+
+l_flat <- loo(mod_flat)
+l_simple <- loo(mod_simple)
+l_base <- loo(mod_base)
+
+## compute waic
+w_simple <- waic(mod_simple)
+w_base <- waic(mod_base)
+
+## compute kfold
+k_simple <- kfold(mod_simple, K=10)
+folds_plz <- kfold_split_stratified(K=10, x=df.model$plz)
+k_base <- kfold(mod_base, K=10, folds=folds_plz)
+
+
+##########################################################################################################
+## save everything
+save(mod_base_default_prior, file="data/mod_base_default_prior.rda")
+save(mod_simple, file="data/mod_simple.rda")
+save(mod_base, file="data/mod_base.rda")
+save(mod_flat, file="data/mod_flat.rda")
+save(l_flat, file="data/loo_flat.rda")
+save(l_simple, file="data/loo_simple.rda")
+save(l_base, file="data/loo_base.rda")
+save(k_simple, file="data/kfold_simple.rda")
+save(k_base, file="data/kfold_base.rda")
+save(w_simple, file="data/waic_simple.rda")
+save(w_base, file="data/waic_base.rda")
+save(df.model, file="data/df_model.rda")
